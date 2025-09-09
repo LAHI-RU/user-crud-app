36f63c2e7cfdd1016dbea9f03ff500bd82108b16
Lahiru Bandara <lahiiru.dananjaya@gmail.com>
2025-09-09 14:44:50 +0530

    Add UserDetails model, migration, factory and seeder

---
+++ app/Models/UserDetails.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserDetails extends Model
+{
+    /** @use HasFactory<\Database\Factories\UserDetailsFactory> */
+    use HasFactory;
+
+    protected $fillable = ['name', 'email', 'phone'];
+}
